94f58c57648812b20d41505436cdd7b8222970e7
pawel <fiderek@gmail.com>
2021-06-08 10:29:09 +0000

    first

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            for(int i=0;i<25;i++)
+                Console.WriteLine(rand.Next(1000,9999));
+        }
+    }
+}
